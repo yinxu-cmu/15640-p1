8329f8a7a5407010cfb8fd08c45f5021a510388b
roddykou <roddykou@gmail.com>
2013-09-10 22:53:55 -0400

    add interface

--- src/Interface/MigratableProcess.java
+++ src/Interface/MigratableProcess.java
@@ -1,5 +1,28 @@
 package Interface;
 
-public class MigratableProcess {
+import java.io.Serializable;
 
+/**
+ * 
+ *
+ */
+public interface MigratableProcess extends Runnable, Serializable {
+	
+	/**
+	 * suspend()
+	 * 
+	 * Called just before the MigratableProcess is serialized to allow
+	 * the process to enter a known, safe state.
+	 */
+	void suspend();
+	
+	/**
+	 * toString()
+	 * 
+	 * Should return the class name concatenated with with all of the arguments
+	 * that were passed to it when it was constructed.
+	 * 
+	 * @return The class name of the process and all of its original arguments.
+	 */
+	String toString();
 }
