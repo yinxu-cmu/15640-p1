05a7fd6709d8ba6cb8897e2acf8e590fc357b8e6
Yin Xu <remonx@gmail.com>
2013-09-12 21:38:02 -0400

    mod io lib

--- src/Classes/TransactionalFileInputStream.java
+++ src/Classes/TransactionalFileInputStream.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-//package Classes;
+package Classes;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/Classes/TransactionalFileOutputStream.java
+++ src/Classes/TransactionalFileOutputStream.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-//package Classes;
+package Classes;
 
 import java.io.IOException;
 import java.io.OutputStream;
